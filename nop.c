c8c77a0b419a288f9a4a9fd5622f499790d72087
alliakorede <alliakoredetaofiq@gmail.com>
2023-06-22 17:23:25 +0100

    files

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+*f_nop- NULL
+*@head: STACK HEAD
+*@counter: LINE NUMBER
+*Return: NO RETURN
+*/
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
